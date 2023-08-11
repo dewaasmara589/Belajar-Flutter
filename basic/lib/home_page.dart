bd9b3c9b0fd3f6d16104baf42770ce6d7ffac74b
Dewa <dewaasmara589@gmail.com>
2023-08-11 10:53:15 +0700

    Basic - add column, row, text, text style, padding

--- basic/lib/home_page.dart
+++ basic/lib/home_page.dart
@@ -6,8 +6,46 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Image.asset(
-            'assets/image.png'
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+                'assets/image.png'
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                ),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Pantai Teluk Penyu",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Cilacap, Jawa Tengah",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
