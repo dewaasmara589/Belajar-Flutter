4c0d0a5a9df817043e110b57ac919e877aec2729
Dewa <dewaasmara589@gmail.com>
2023-08-14 16:58:15 +0700

    Codelab Flutter Pemula Dicoding

---
+++ codelab_1/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:code_lab_1/detail_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: DetailScreen(),
+    );
+  }
+}
