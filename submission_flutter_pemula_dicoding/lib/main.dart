d96c95a82150681c16f964656ea8764dc84bd330
Dewa <dewaasmara589@gmail.com>
2023-10-05 14:49:56 +0700

    submission_flutter_pemula_dicoding : Complete make MenuText to Filter Grid List Makanan

--- submission_flutter_pemula_dicoding/lib/main.dart
+++ submission_flutter_pemula_dicoding/lib/main.dart
@@ -236,96 +236,6 @@ class MainScreen extends StatelessWidget {
           ),
           const SizedBox(height : 10.0),
           const MenuText(titleMenu: ['All', 'Appetizer', 'Main Course', 'Dessert']),
-          const SizedBox(height: 5.0,),
-
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 2,
-              padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-              crossAxisSpacing: 0.0,
-              mainAxisSpacing: 0.0,
-              children: listDataModel.map((data) => Card(
-                child: Container(
-                  alignment: Alignment.center,// use aligment
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    // border: Border.all(color: Colors.black),
-                  ),
-                  child: InkWell(
-                    onTap: (){
-                      print('Clicked : ' + data.id.toString());
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        ClipRRect(
-                          borderRadius: const BorderRadius.all(
-                              Radius.circular(5.0)
-                          ),
-                          child : Image.asset(
-                            data.image,
-                            height: 85,
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                        const SizedBox(height: 3,),
-                        Text(data.title,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(data.subTitle,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            color : Colors.grey,
-                            fontSize: 12.0,
-                          ),
-                        ),
-                        const SizedBox(height: 3,),
-                        Row(
-                          crossAxisAlignment : CrossAxisAlignment.center,
-                          mainAxisAlignment : MainAxisAlignment.center,
-                          children: [
-                            const Icon(
-                              Icons.star,
-                              color: Colors.orange,
-                              size: 15.0,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                              child : Text(data.rating.toString(), style: const TextStyle(fontSize: 12.0),),
-                            ),
-                            const Text('|'),
-                            const SizedBox(width: 2.0,),
-                            const Icon(
-                              Icons.delivery_dining_sharp,
-                              color: Colors.orange,
-                              size: 15.0,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                              child : Text(data.duration, style: const TextStyle(fontSize: 12.0),),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 3,),
-                        Text(data.price,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),).toList(),
-            ),
-          ),
-
         ],
       ),
     );
@@ -346,65 +256,186 @@ class _MenuText extends State<MenuText> {
     0 : true,
     1 : false,
     2 : false,
-    3 : false,
+    3 : false
   };
 
   var getKey = 0;
+  var selectedValue = 'All';
+
+  late List<dataModel> newDataList = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      height: 30.0,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        itemCount: widget.titleMenu.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget> [
-                  Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 2, color: getKey == index? Colors.brown : Colors.transparent,),
-                        borderRadius: BorderRadius.all(Radius.circular(5)),
-                      ),
-                      child : Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5.0),
-                          child: Text(
-                            widget.titleMenu[index].toString(),
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 14.0,
-                            ),
+    if (selectedValue == 'All'){
+      newDataList.clear();
+      newDataList = List.from(listDataModel);
+    }
+    return Column(
+      children: [
+        Container(
+        height: 30.0,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            itemCount: widget.titleMenu.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget> [
+                      Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 2, color: getKey == index? Colors.brown : Colors.transparent,),
+                            color: getKey == index ? const Color.fromRGBO(128, 128, 128, 0.2) : Colors.transparent,
+                            borderRadius: const BorderRadius.all(Radius.circular(5)),
+                          ),
+                          child : Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5.0),
+                              child: Text(
+                                widget.titleMenu[index].toString(),
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14.0,
+                                ),
+                              )
                           )
                       )
-                  )
-                ],
-              ),
-            onTap : () {
-              menuTextIsPressed.updateAll((key, value){
-                if(key == index){
-                  return true;
-                }else{
-                  return false;
-                }
-              });
+                    ],
+                  ),
+                  onTap : () {
+                    menuTextIsPressed.updateAll((key, value){
+                      if(key == index){
+                        return true;
+                      }else{
+                        return false;
+                      }
+                    });
 
 
-              print('List = ' + menuTextIsPressed.toString());
-              var trueKey = menuTextIsPressed.keys.firstWhere((k) => menuTextIsPressed[k] == true);
+                    // Trigger MenuText
+                    var trueKey = menuTextIsPressed.keys.firstWhere((k) => menuTextIsPressed[k] == true);
 
-              setState(() {
-                getKey = trueKey;
-              });
+                    setState(() {
+                      selectedValue = widget.titleMenu[index].toString();
+                      getKey = trueKey;
 
-              print('Tes = ' + trueKey.toString());
+                      if (selectedValue == 'All'){
+                        newDataList.clear();
+                        newDataList = List.from(listDataModel);
+                      }else{
+                        newDataList.clear();
+                        for (var item in listDataModel) {
+                          if (selectedValue == item.type){
+                            newDataList.add(dataModel(id: item.id, image: item.image,
+                                title: item.title, subTitle: item.subTitle,
+                                rating: item.rating, duration: item.duration,
+                                price: item.price, type: item.type, desc: item.desc));
+                          }
+                        }
+                      }
+                    });
+                  }
+              );
             }
-          );
-        }
-      ),
+          )
+        ),
+        const SizedBox(height : 10.0),
+        Container(
+          height: 398.0,
+          child: GridView.count(
+            crossAxisCount: 2,
+            padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+            crossAxisSpacing: 0.0,
+            mainAxisSpacing: 0.0,
+            children: newDataList.map((data) =>
+            Card(
+              child: Container(
+                alignment: Alignment.center, // use aligment
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  // border: Border.all(color: Colors.black),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    print('Clicked : ' + data.id.toString());
+                    print('Clicked : ' + data.title.toString());
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.all(
+                            Radius.circular(5.0)
+                        ),
+                        child: Image.asset(
+                          data.image,
+                          height: 85,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      const SizedBox(height: 3,),
+                      Text(data.title,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(data.subTitle,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 12.0,
+                        ),
+                      ),
+                      const SizedBox(height: 3,),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            color: Colors.orange,
+                            size: 15.0,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 2.0),
+                            child: Text(data.rating.toString(),
+                              style: const TextStyle(fontSize: 12.0),),
+                          ),
+                          const Text('|'),
+                          const SizedBox(width: 2.0,),
+                          const Icon(
+                            Icons.delivery_dining_sharp,
+                            color: Colors.orange,
+                            size: 15.0,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 2.0),
+                            child: Text(data.duration,
+                              style: const TextStyle(fontSize: 12.0),),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 3,),
+                      Text(data.price,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )).toList(),
+          ),
+        )
+      ],
     );
   }
 }
