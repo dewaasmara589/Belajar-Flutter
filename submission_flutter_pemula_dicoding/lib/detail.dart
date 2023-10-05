5349c4d7dbdd4f9484c38f66889fac6f324bbb5d
Dewa <dewaasmara589@gmail.com>
2023-10-05 17:02:58 +0700

    submission_flutter_pemula_dicoding : Add Title, Image, and Rating in Detail Class

--- submission_flutter_pemula_dicoding/lib/detail.dart
+++ submission_flutter_pemula_dicoding/lib/detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:submission_flutter_pemula_dicoding/data.dart';
 
 void main() => runApp(detail(idSelectedListMakanan: null, nameSelectedListMakanan: null));
 
@@ -10,27 +11,93 @@ class detail extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    var imageSelected;
+    var subTitleSelected;
+    var ratingSelected;
+
+    for (var item in listDataModel) {
+      if (idSelectedListMakanan.toString() == item.id.toString()){
+        imageSelected = item.image;
+        subTitleSelected = item.subTitle;
+        ratingSelected = item.rating.toString();
+      }
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
       home:  Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.brown,
+          title: const Text('WM Spesial Wuenak'),
+        ),
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                idSelectedListMakanan,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10.0),
+                child : Column (
+                  children: [
+                    Text(
+                      nameSelectedListMakanan,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.brown,
+                      ),
+                    ),
+                    Text(
+                      '(' + subTitleSelected + ')',
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Text(
-                nameSelectedListMakanan,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
+              Container(
+                height: 230.0,
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(
+                      Radius.circular(5.0)
+                  ),
+                  child : Image.asset(
+                    imageSelected,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40.0,),
+              Container(
+                margin: const EdgeInsets.fromLTRB(10.0, 0, 0, 0),
+                child: Row(
+                  children: [
+                    const Text(
+                      "Food Details",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(width: 20.0,),
+                    const Icon(
+                      Icons.star,
+                      color: Colors.orange,
+                      size: 20.0,
+                    ),
+                    const SizedBox(width: 2.0,),
+                    Text(
+                      '(' + ratingSelected + ')',
+                      style: const TextStyle(
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
