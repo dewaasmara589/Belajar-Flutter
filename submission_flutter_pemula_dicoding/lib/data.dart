86c8453b544def54610dcb3fca20fcc4eeb8f449
Dewa <dewaasmara589@gmail.com>
2023-10-06 10:30:55 +0700

    submission_flutter_pemula_dicoding : Complete Layout Detail Class

--- submission_flutter_pemula_dicoding/lib/data.dart
+++ submission_flutter_pemula_dicoding/lib/data.dart
@@ -3,7 +3,7 @@ import 'package:submission_flutter_pemula_dicoding/dataModel.dart';
 List<dataModel> listDataModel = [
   dataModel(id: 1, image: "assets/images/content2.jpg",
       title: "French Croquette", subTitle: "Kroket", rating: 4.7,
-      duration: "10 min", price: "Rp 36.000", type: "Appetizer",
+      duration: "10 min", price: "36000", type: "Appetizer",
       desc: "French Croquette atau di Indonesia lebih dikenal dengan "
           "sebutan kroket adalah makanan berbahan dasar kentang. "
           "Selain kentang, di dalamnya dicampurkan juga daging lalu "
@@ -12,7 +12,7 @@ List<dataModel> listDataModel = [
           "Belanda ini biasanya dibungkus menggunakan kertas krep"),
   dataModel(id: 2, image: "assets/images/content3.jpg",
       title: "Gulai", subTitle: "Ikan Patin", rating: 4.8,
-      duration: "30 min", price: "Rp 30.000", type: "Main Course",
+      duration: "30 min", price: "30000", type: "Main Course",
       desc: "Seperti namanya, gulai yang satu ini berbahan dasar ikan patin "
           "yang diberi bumbu-bumbu dan rempah kemudian dimasak. Tak hanya "
           "lezat, makanan ini juga kaya akan gizi.Gulai ikan patin cocok "
@@ -20,25 +20,25 @@ List<dataModel> listDataModel = [
           "goreng dan nasi putih."),
   dataModel(id: 3, image: "assets/images/content4.jpg",
       title: "Mie", subTitle: "Bangka", rating: 4.9,
-      duration: "10 min", price: "Rp 36.000", type: "Main Course",
+      duration: "10 min", price: "36000", type: "Main Course",
       desc: "Mi Bangka atau dikenal juga dengan nama bakmi Bangka "
           "merupakan hidangan tradisional dari Provinsi Bangka Belitung. "
           "Kuliner ini biasanya ditambah dengan aneka topping seperti "
           "bakso sapi, udang, irisan ayam, dan lainnya."),
   dataModel(id: 4, image: "assets/images/content5.jpg",
       title: "Ayam", subTitle: "Betutu", rating: 5.0,
-      duration: "90 min", price: "Rp 52.000", type: "Main Course",
+      duration: "90 min", price: "52000", type: "Main Course",
       desc: "Ayam betutu merupakan hidangan khas Bali, terbuat dari daging "
           "ayam utuh dan dimasak dengan bumbu base genep."),
   dataModel(id: 5, image: "assets/images/content6.jpg",
       title: "Ayam", subTitle: "Taliwang", rating: 4.9,
-      duration: "80 min", price: "Rp 55.000", type: "Main Course",
+      duration: "80 min", price: "55000", type: "Main Course",
       desc: "Ayam Taliwang adalah makanan khas yang berasal dari Karang "
           "Taliwang, Mataram, Nusa Tenggara Barat. Makanan ini berbahan "
           "dasar daging ayam."),
   dataModel(id: 6, image: "assets/images/content7.jpg",
       title: "Soto", subTitle: "Banjar", rating: 4.8,
-      duration: "15 min", price: "Rp 25.000", type: "Main Course",
+      duration: "15 min", price: "25000", type: "Main Course",
       desc: "Soto Banjar adalah soto ayam berempah yang lahir di tengah "
           "masyarakat suku Banjar di Kalimantan Selatan. Bisa dibilang juga "
           "bahkan hidangan ini adalah salah satu masakan terpopuler "
@@ -50,26 +50,26 @@ List<dataModel> listDataModel = [
           "susu ke dalam kaldunya sehinga kuah soto nampak lebih keruh."),
   dataModel(id: 7, image: "assets/images/content8.jpg",
       title: "Soto", subTitle: "Betawi", rating: 4.8,
-      duration: "17 min", price: "Rp 28.000", type: "Main Course",
+      duration: "17 min", price: "28000", type: "Main Course",
       desc: "Soto Betawi merupakan salah satu jenis soto khas Nusantara "
           "yang paling digemari. Berkuah putih dan cukup kental, soto Betawi "
           "ada yang terbuat dari santan kelapa atau susu."),
   dataModel(id: 8, image: "assets/images/content9.jpg",
       title: "Turkish Baklava", subTitle: "with Turkish Tea", rating: 4.6,
-      duration: "10 min", price: "Rp 23.000", type: "Dessert",
+      duration: "10 min", price: "23000", type: "Dessert",
       desc: "sejenis makanan ringan di kawasan Turki dan daerah-daerah "
           "tempat mantan kekuasaan Kesultanan Utsmaniyah. Makanan ini "
           "terdiri dari kacang walnut atau pistache yang dicincang dan "
           "diberi pemanis (gula atau madu) dan dibungkus adonan roti tipis"),
   dataModel(id: 9, image: "assets/images/content10.jpg",
       title: "Salad", subTitle: "Buah, Yoghurt", rating: 4.8,
-      duration: "5 min", price: "Rp 28.000", type: "Appetizer",
+      duration: "5 min", price: "28000", type: "Appetizer",
       desc: "Jika Anda sedang ingin memaksimalkan program diet, salad buah "
           "yoghurt merupakan salah satu menu yang pas disantap sebagai "
           "hidangan penutup Anda."),
   dataModel(id: 10, image: "assets/images/content11.jpg",
       title: "Gudeg", subTitle: "Yogyakarta", rating: 5.0,
-      duration: "30 min", price: "Rp 40.000", type: "Main Course",
+      duration: "30 min", price: "40000", type: "Main Course",
       desc: "Gudeg merupakan masakan yang berbahan utama nangka muda atau "
           "oleh warga lokal disebut gori. Dalam prosesnya, potongan daging "
           "nangka ini direbus dengan gula merah dan santan dengan api kecil "
--- submission_flutter_pemula_dicoding/lib/detail.dart
+++ submission_flutter_pemula_dicoding/lib/detail.dart
@@ -14,12 +14,16 @@ class detail extends StatelessWidget{
     var imageSelected;
     var subTitleSelected;
     var ratingSelected;
+    var descriptionSelected;
+    var priceSelected;
 
     for (var item in listDataModel) {
       if (idSelectedListMakanan.toString() == item.id.toString()){
         imageSelected = item.image;
         subTitleSelected = item.subTitle;
         ratingSelected = item.rating.toString();
+        descriptionSelected = item.desc.toString();
+        priceSelected = item.price.toString();
       }
     }
 
@@ -72,7 +76,7 @@ class detail extends StatelessWidget{
                   ),
                 ),
               ),
-              const SizedBox(height: 40.0,),
+              Spacer(),
               Container(
                 margin: const EdgeInsets.fromLTRB(10.0, 0, 0, 0),
                 child: Row(
@@ -100,10 +104,142 @@ class detail extends StatelessWidget{
                   ],
                 ),
               ),
+              const SizedBox(height: 3.0,),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                child : Text(
+                  descriptionSelected,
+                  style: const TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              ),
+              const Spacer(flex: 2,),
+              Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox.fromSize(
+                        size: const Size(30, 30),
+                        child: ClipOval(
+                          child: Material(
+                            color: Colors.orange,
+                            child: InkWell(
+                                splashColor: Colors.yellow,
+                                onTap: () {
+
+                                },
+                                child: const Center(
+                                  child: Text(
+                                    "-",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 26.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                )
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 12,),
+                      const Text(
+                        "1",
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      const SizedBox(width: 12,),
+                      SizedBox.fromSize(
+                        size: const Size(30, 30),
+                        child: ClipOval(
+                          child: Material(
+                            color: Colors.orange,
+                            child: InkWell(
+                                splashColor: Colors.yellow,
+                                onTap: () {},
+                                child: const Center(
+                                  child: Text(
+                                    "+",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 23.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                )
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20,),
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 30.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Text(
+                            priceSelected,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Stack(
+                              children: <Widget>[
+                                Positioned.fill(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      color: Colors.orange,
+                                    ),
+                                  ),
+                                ),
+                                TextButton(
+                                  style: TextButton.styleFrom(
+                                    foregroundColor: Colors.white,
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 45.0,
+                                      vertical: 10.0,
+                                    ),
+                                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                    textStyle: const TextStyle(fontSize: 14),
+                                  ),
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Add to Cart',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16.0,
+                                    ),),
+                                ),
+                              ],
+                            ),
+                          )
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
   }
+
+
 }
